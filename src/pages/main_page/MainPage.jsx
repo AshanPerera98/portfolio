b9cedba8a1d3f53479847550a954cd9ccb49f7d2
Ashan Perera <ashantiwankaperera@gmail.com>
2024-05-26 15:31:00 +0530

    medium test

--- src/pages/main_page/MainPage.jsx
+++ src/pages/main_page/MainPage.jsx
@@ -3,8 +3,15 @@ import Hero from "../../sections/hero_section/Hero";
 import AboutMe from "../../sections/about_me/AboutMe";
 import Experience from "../../sections/experience/Experience";
 import Education from "../../sections/education/Education";
+import parse from "rss-to-json";
+import { useEffect } from "react";
 
 function MainPage() {
+  useEffect(() => {
+    parse("https://medium.com/feed/@ashantiwankaperera").then((rss) => {
+      console.log(JSON.stringify(rss, null, 3));
+    });
+  }, []);
   return (
     <div className="MainPage">
       <Hero />
