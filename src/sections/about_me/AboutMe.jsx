7bb3cfef43d8f949d341464c02dae1e6806b4674
Ashan Perera <ashantiwankaperera@gmail.com>
2024-05-29 21:39:47 +0530

    bio updated

--- src/sections/about_me/AboutMe.jsx
+++ src/sections/about_me/AboutMe.jsx
@@ -60,12 +60,14 @@ function AboutMe() {
           </div>
           <div className="column has-text-left">
             <p>
-              t Ut enim ad minim veniam, quis nostrud exercitation ullamco
-              laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-              dolor in reprehenderit in voluptate velit esse cillum dolore eu
-              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-              proident, sunt in culpa qui officia deserunt mollit anim id est
-              laborum.
+              Multi-faceted developer and designer with expertise in software
+              engineering specialized in fron-tend development, UI/UX, graphic
+              design, and video editing. Skilled in crafting logos, branding
+              elements, and user interfaces that resonate with target audiences.
+              Leverage more than 3 years of front-end development experience and
+              nearly half-decade of designing experience to translate user needs
+              into intuitive and visually compelling products, marketing
+              materials, and digital experiences.
             </p>
             <div className="columns details-section">
               <div className="column">
